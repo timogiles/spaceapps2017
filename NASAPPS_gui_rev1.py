0d6e4d8623525cfe65e1ff407d6a575700da13b0
timogiles <timothy.j.giles@gmail.com>
2017-04-30 10:37:58 -0400

    icon removal, weather
    icons and thier power consumption are now removable by using the minus
    signs.  I added some "hitbox" code to make click detection a little
    easier to code.  Weather info is now printed at the top and different
    solar weather can be selected by clicking.  Haven't integrated the
    effects of those clicks into the graph yet.

--- NASAPPS_gui_rev1.py
+++ NASAPPS_gui_rev1.py
@@ -8,6 +8,7 @@ from scipy import linspace, polyval, polyfit, sqrt, stats, randn
 from scipy.integrate import quad , simps
 from random import randint
 import os
+import ephem
 
 #pygame initialization-type tasks
 pygame.init()
@@ -20,58 +21,30 @@ fps = pygame.time.Clock()
 screenlength = 1366
 screenwidth = 768
 
+BatteryCapacity = 50
+BatteryStart = 30
+Weather = ["Sunny", "Partly Cloudy", "Cloudy", "Rainy"]
+WeatherRating = [1,.75,.35,.1]
+WeatherIndex = 0
+lat = '27.9621333'
+lon = '-82.4448439'
+
+#hitbox centers
+laptopMinus = [35,724]
+coffeeMinus = [35,653]
+fridgeMinus = [35,578]
+phoneMinus = [35,502]
+lightMinus = [35,417]
+dryerMinus = [35,334]
+washerMinus = [35,260]
+WeatherHitbox = [133,38]
+
 #we create the primary window object and fill it with white
 window = pygame.display.set_mode((screenlength,screenwidth), pygame.FULLSCREEN)
 
 window.fill(pygame.Color(255,255,255))
 
-def Calculate_Sun_p(CellStart,CellStop, \
-                  LightingStart, LightingStop, \
-                  CoffeeStart, CoffeeStop, \
-                  DryerStart,DryerStop, \
-                  WasherStart, WasherStop, \
-                  FridgeStart, FridgeStop, \
-                  LaptopStart, LaptopStop):
-
-    
-    
-	
-    out1 = []
-    out1.append([0, 27.3388888888889, 0])
-    out1.append([0.0416666666666667, 24.6777777777778, 0])
-    out1.append([0.0833333333333333, 22.0166666666667, 0])
-    out1.append([0.125, 19.3555555555556, 0])
-    out1.append([0.166666666666667, 16.6944444444444, 0])
-    out1.append([0.208333333333333, 14.0333333333333, 0])
-    out1.append([0.25, 11.3722222222222, 0])
-    out1.append([0.291666666666667, 10.2219500964689, 1.34417231869112])
-    out1.append([0.333333333333333, 10.1375278343533, 2.63668884899547])
-    out1.append([0.375, 11.2442956287472, 3.82787890550506])
-    out1.append([0.416666666666667, 13.3951502400114, 4.87196572237531])
-    out1.append([0.458333333333333, 16.4028647576648, 5.72882562876454])
-    out1.append([0.5, 20.0472836255565, 6.36552997900277])
-    out1.append([0.541666666666667, 24.0837830964236, 6.75761058197826])
-    out1.append([0.583333333333333, 28.2526719853125, 6.89])
-    out1.append([0.625, 32.2891714561797, 6.75761058197826])
-    out1.append([0.666666666666667, 35.9335903240713, 6.36552997900277])
-    out1.append([0.708333333333333, 38.9413048417248, 5.72882562876454])
-    out1.append([0.75, 41.092159452989, 4.87196572237531])
-    out1.append([0.791666666666667, 42.1989272473829, 3.82787890550506])
-    out1.append([0.833333333333333, 41.8145049852673, 2.63668884899547])
-    out1.append([0.875, 37.9375661928473, 1.34417231869113])
-    out1.append([0.916666666666667, 34.7164550817362, 8.44127285432039E-16])
-    out1.append([0.958333333333333, 32.0553439706251, 0])
-    out1.append([1, 29.3942328595139, 0])
-
-    return out1
-
-def Calculate_Sun(BatteryStart,phone_plugin,phone_unplug, \
-                  light_plugin,light_unplug, \
-                  coffee_plugin,coffee_unplug, \
-                  dryer_plugin,dryer_unplug, \
-                  washer_plugin,washer_unplug, \
-                  fridge_plugin,fridge_unplug, \
-                  laptop_plugin,laptop_unplug):
+def Calculate_Sun(BatteryStart):
     #kWh usage of each item  
     phone_kwh = .03
     light_kwh = .15
@@ -139,21 +112,24 @@ def Calculate_Sun(BatteryStart,phone_plugin,phone_unplug, \
     out1 = BatteryStart
     for i in range(len(sun)):
         out1 += sun[i]
-        if (i >= phone_plugin and i < phone_unplug):
+        if (i >= phone_plugin[1] and i < phone_unplug[1] and phone_plugin[2] == 1):
             out1 -= phone_kwh
-        if (i >= light_plugin and i < light_unplug):
+        if (i >= light_plugin[1] and i < light_unplug[1] and light_plugin[2] == 1):
             out1 -= light_kwh
-        if (i >= coffee_plugin and i < coffee_unplug):
+        if (i >= coffee_plugin[1] and i < coffee_unplug[1] and coffee_plugin[2] == 1):
             out1 -= coffee_kwh
-        if (i >= dryer_plugin and i < dryer_unplug):
+        if (i >= dryer_plugin[1] and i < dryer_unplug[1] and dryer_plugin[2] == 1):
             out1 -= dryer_kwh
-        if (i >= washer_plugin and i < washer_unplug):
+        if (i >= washer_plugin[1] and i < washer_unplug[1] and washer_plugin[2] == 1):
             out1 -= washer_kwh
-        if (i >= fridge_plugin and i < fridge_unplug):
+        if (i >= fridge_plugin[1] and i < fridge_unplug[1] and fridge_plugin[2] == 1):
             out1 -= fridge_kwh
-        if (i >= laptop_plugin and i < laptop_unplug):
+        if (i >= laptop_plugin[1] and i < laptop_unplug[1] and laptop_plugin[2] == 1):
             out1 -= laptop_kwh
             
+        #check for battery full
+        if (out1 > BatteryCapacity):
+            out1 = BatteryCapacity
         outval.append([time[i],sun[i],out1]) 
 
     return outval
@@ -162,13 +138,7 @@ def updateGraph():
     #redraw the graph picture
     window.blit(graph,(250,10))
     #calculate power usage
-    retval = Calculate_Sun(50,phone_plugin[1],phone_unplug[1], \
-                           light_plugin[1],light_unplug[1], \
-                           coffee_plugin[1],coffee_unplug[1], \
-                           dryer_plugin[1],dryer_unplug[1], \
-                           washer_plugin[1],washer_unplug[1], \
-                           fridge_plugin[1],fridge_unplug[1], \
-                           laptop_plugin[1],laptop_unplug[1])
+    retval = Calculate_Sun(BatteryStart)
     #determine graph scale
     maxnum=0
     for i in range(len(retval)):
@@ -201,6 +171,27 @@ def ClickToHours(pos):
     valid = 1
     retval = [new_pos, new_hours, valid]
     return retval
+    
+def CheckHitbox(location, mouseX, mouseY):
+    if (mouseX>=location[0]-25 and mouseX<=location[0]+25 \
+    and mouseY>=location[1]-25 and mouseY<=location[1]+25):
+        return 1
+    else:
+        return 0
+
+def GetSun_rise_set():
+    o=ephem.Observer()
+    o.lat = lat
+    o.long = lon
+    s=ephem.Sun()
+    s.compute()
+    
+    sunrise =  ephem.localtime(o.previous_rising(s))
+    sunset =  ephem.localtime(o.next_setting(s))
+    sunrise = str(sunrise)[11:19]
+    sunset = str(sunset)[11:19]
+
+    return [sunrise, sunset]
 
 #we initiate pygame
 pygame.font.init()
@@ -342,6 +333,18 @@ def RedrawScreen():
     if (washer_plugin[2] == 1):
         window.blit(washer_plugin_image,(washer_plugin[0],230))
         window.blit(washer_unplug_image,(washer_unplug[0],230))
+        
+    ##weather and sun info
+    label = myFont.render("Weather is " + Weather[WeatherIndex],1,(255,255,255))
+    window.blit(label,(20,20))
+    label = myFont.render("Today is Sunday, April 30th",1,(255,255,255))
+    window.blit(label,(20,40))
+    Sunrise_set = GetSun_rise_set()
+    label = myFont.render("Sun Rises at " + Sunrise_set[0],1,(255,255,255))
+    window.blit(label,(20,60))
+    label = myFont.render("Sun Sets at " + Sunrise_set[1],1,(255,255,255))
+    window.blit(label,(20,80))
+
 
 RedrawScreen()        
 
@@ -367,7 +370,10 @@ while True:
                 quit()
 
             ### LAPTOP ###
-
+            if CheckHitbox(laptopMinus, mouseX, mouseY):
+                laptop_plugin = [0,0,0]
+                RedrawScreen()
+                
             if (mouseX>=150 and mouseX<=213 and mouseY>=697 and mouseY<=749):
                 print "add laptop"
 
@@ -410,6 +416,9 @@ while True:
                 RedrawScreen()
 
             ### COFFEE ###
+            if CheckHitbox(coffeeMinus, mouseX, mouseY):
+                coffee_plugin = [0,0,0]
+                RedrawScreen()
             
             if (mouseX>=142 and mouseX<=188 and mouseY>=631 and mouseY<=675):
                 print "add coffee"
@@ -454,7 +463,10 @@ while True:
                 RedrawScreen()
                 
             ### FRIDGE ###
-            
+            if CheckHitbox(fridgeMinus, mouseX, mouseY):
+                fridge_plugin = [0,0,0]
+                RedrawScreen()
+
             if (mouseX>=142 and mouseX<=195 and mouseY>=552 and mouseY<=608):
                 print "add fridge"
 
@@ -498,7 +510,10 @@ while True:
                 RedrawScreen()
 
             ### PHONE ###
-            
+            if CheckHitbox(phoneMinus, mouseX, mouseY):
+                phone_plugin = [0,0,0]
+                RedrawScreen()
+          
             if (mouseX>=140 and mouseX<=192 and mouseY>=478 and mouseY<=527):
                 print "add phone"
 
@@ -541,6 +556,9 @@ while True:
                 RedrawScreen()
 
             ### LIGHT ###
+            if CheckHitbox(lightMinus, mouseX, mouseY):
+                light_plugin = [0,0,0]
+                RedrawScreen()
             
             if (mouseX>=141 and mouseX<=195 and mouseY>=392 and mouseY<=446):
                 print "add light"
@@ -585,7 +603,10 @@ while True:
                 RedrawScreen()
 
             ### DRYER ###
-            
+            if CheckHitbox(dryerMinus, mouseX, mouseY):
+                dryer_plugin = [0,0,0]
+                RedrawScreen()
+           
             if (mouseX>=142 and mouseX<=196 and mouseY>=307 and mouseY<=359):
                 print "add dryer"
 
@@ -627,9 +648,21 @@ while True:
                 #window.blit(dryer_unplug_image,(dryer_unplug[0],305))
                 #updateGraph()
                 RedrawScreen()
+                
+            ### Weather ###
+            if CheckHitbox(WeatherHitbox, mouseX, mouseY):
+                WeatherIndex += 1
+                if WeatherIndex > 3:
+                    WeatherIndex = 0
 
+                print "WeatherIndex" + str(WeatherIndex)
+                RedrawScreen()
+                
             ### WASHER ###
-            
+            if CheckHitbox(washerMinus, mouseX, mouseY):
+                washer_plugin = [0,0,0]
+                RedrawScreen()
+
             if (mouseX>=141 and mouseX<=193 and mouseY>=234 and mouseY<=282):
                 print "add dryer"
 
